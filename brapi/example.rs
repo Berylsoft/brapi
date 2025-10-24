0a5f7305b6e994b1429e32b0e6fa3523cd589e16
stackinspector <stackinspector@126.com>
2025-10-24 16:25:46 +0800

    example BAPI_ACCESS_PATH

--- brapi/example.rs
+++ brapi/example.rs
@@ -1,6 +1,8 @@
 #[tokio::main]
 async fn main() {
-    let client = brapi::client::Client::bare();
+    let access_path = std::env::var_os("BAPI_ACCESS_PATH").unwrap();
+    let access = std::fs::read_to_string(access_path).unwrap();
+    let client = brapi::client::Client::with_access(access, None).unwrap();
     let res = client.call(&brapi::wbi_api::GetBasicInfo).await;
     let _ = dbg!(res);
 }
