5d4b6dc2b0ef23553b3ee956c321e99cfe69f224
stackinspector <stackinspector@126.com>
2025-10-26 03:30:22 +0800

    example

--- brapi/example.rs
+++ brapi/example.rs
@@ -3,6 +3,7 @@ async fn main() {
     let access_path = std::env::var_os("BAPI_ACCESS_PATH").unwrap();
     let access = std::fs::read_to_string(access_path).unwrap();
     let client = brapi::client::Client::with_access(access, None).unwrap();
-    let res = client.call(&brapi::wbi_api::GetBasicInfo).await;
+    client.update_wbi_key().await.unwrap();
+    let res = client.call(&brapi::live::feed::GetHostsInfo { roomid: 5440 }).await.unwrap();
     let _ = dbg!(res);
 }
